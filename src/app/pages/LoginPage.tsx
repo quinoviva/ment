58732c1647e29ea2fb4498eb6faa5efb39b61ce5
itwasallinmyhead7 <itwasallinmyhead7@gmail.com>
2026-03-18 11:14:04 +0000

    fixed: login page

--- src/app/pages/LoginPage.tsx
+++ src/app/pages/LoginPage.tsx
@@ -11,11 +11,7 @@ import { TreePine, Lock, User } from 'lucide-react';
 // Import Firebase functions and config
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { getFirestore, collection, query, where, getDocs, doc, getDoc } from 'firebase/firestore'; // Firestore imports
-<<<<<<< HEAD
-import { app } from "../../firebase/firebase";
-=======
 import { app } from '../../firebase/firebase'; // Import initialized app
->>>>>>> 4fecdb78710441e337feaf67abf3ce0f9e71046c
 import { storage, User as AppUser } from '../utils/storage'; // Rename User to AppUser to avoid conflict
 
 const auth = getAuth(app); // Initialize Firebase Auth
